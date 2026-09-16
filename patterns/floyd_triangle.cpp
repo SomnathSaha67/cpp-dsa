2d075e63abfe8f49e78ee93876bb6f5a756feaf0
SomnathSaha2006 <ss0326736@gmail.com>
2026-09-16 19:53:49 +0530

    Floyd's triangle pattern

---
+++ patterns/floyd_triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n, num= 1;
+
+  cout<<"Enter value of n: ";
+  cin>>n;
+
+  for (int i=0; i<n; i++){
+    for (int j=0; j<i+1; j++){
+      cout<<num<<" ";
+      num++;
+    }
+    cout<<"\n";
+  }
+
+  return 0;
+}
