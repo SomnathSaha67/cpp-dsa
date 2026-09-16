96d5fba3931bd96f189520ffc4171a82c934a9e0
SomnathSaha2006 <ss0326736@gmail.com>
2026-09-16 20:12:06 +0530

    inverted triangle pattern

---
+++ patterns/inverted_traingle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n; char ch= 'A';
+
+  cout<<"Enter value of n: ";
+  cin>>n;
+
+  for (int i=0; i<n; i++){
+    for (int j=0; j<i; j++){
+      cout<<" ";
+    }
+    for (int k=0; k<n-i; k++){
+      cout<<ch;
+    }
+    ch++;
+    cout<<"\n";
+  }
+
+  return 0;
+}
